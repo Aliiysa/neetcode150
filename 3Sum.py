fcdcfe1f143c510f2a1b369bf4a92349d0d05a75
Ali Sabanci <alisabanci7@gmail.com>
2023-11-13 11:13:47 +0300

    upload

---
+++ 3Sum.py
@@ -0,0 +1,21 @@
+def threeSum(nums):
+    nums.sort()
+    result = []
+
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            threeSum = a + nums[left] + nums[right]
+            if threeSum < 0:
+                left += 1
+            elif threeSum > 0:
+                right -= 1
+            else:
+                result.append([a, nums[left], nums[right]])
+                left += 1
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
+    return result
+    
