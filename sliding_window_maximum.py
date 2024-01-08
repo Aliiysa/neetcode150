1848dc67dc1613ae199078f636bd1aa4333e423f
Ali Sabanci <alisabanci7@gmail.com>
2024-01-08 16:56:55 +0300

    upload

---
+++ sliding_window_maximum.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def maxSlidingWindow(nums, k):
+    res = []
+    l = r = 0
+    q = deque()
+
+    while r < len(nums):
+        while q and nums[q[-1]] < nums[r]:
+            q.pop()
+        q.append(r)
+
+        if l > q[0]:
+            q.popleft()
+
+        if (r + 1) >= k:
+            res.append(nums[q[0]])
+            l += 1
+        
+        r += 1
+    return res
